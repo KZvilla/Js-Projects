38341b812d02d95190068488e233228be405bcfd
KZvilla <40326801+KZvilla@users.noreply.github.com>
2021-07-30 01:25:31 -0300

    final proyect

--- JSP/Roman Numeral Converter.js
+++ JSP/Roman Numeral Converter.js
@@ -1,5 +1,25 @@
 function convertToRoman(num) {
-    return num;
+    if (!+num){return false;}		
+       let	digits = String(num).split(""),
+           romanSymbols = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+                  "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+                  "","I","II","III","IV","V","VI","VII","VIII","IX"],
+           romanNum = "",
+           i = 3;
+       while (i--)
+           romanNum = (romanSymbols[+digits.pop() + (i * 10)] || "") + romanNum;
+       return Array(+digits.join("") + 1).join("M") + romanNum;
    }
    
-   convertToRoman(36);
+   console.log(convertToRoman(3999));
+function separateUnits(num) { 
+    let number = num.toString().split('');
+    let multiplier = 1;  
+
+    for (let i = number.length - 1; i >= 0; i--) {
+        number[i] *= multiplier;
+        multiplier *= 10;
+    }
+    return number;
+}
+separateUnits(2134);
